919d8043ad45e3e1b8999af93d27818ec9425102
lopez <patrice.lopez@science-miner.com>
2018-09-29 01:24:18 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,89 @@
+# Simple python client for GROBID REST services
+
+This Python client can be used to process in an efficient concurrent manner a set of PDF in a given directory by the [GROBID](https://github.com/kermitt2/grobid) service. Results are written in a given output directory and include the resulting XML TEI representation of the PDF. 
+
+## Build and run
+
+You need first to install and start the *grobid* service, latest stable version, see the [documentation](http://grobid.readthedocs.io/). It is assumed that the server will run on the address `http://localhost:8070`. You can change the server address by editing the file `config.json`.
+
+## Requirements
+
+This client has been developed and tested with Python 3.5.
+
+## Install
+
+Get the github repo:
+
+> git clone https://github.com/kermitt2/grobid-client-python
+
+> cd grobid-client-python
+
+It is advised to setup first a virtual environment to avoid falling into one of these gloomy python dependency marshlands:
+
+> virtualenv --system-site-packages -p python3 env
+
+> source env/bin/activate
+
+Install the dependencies, use:
+
+> pip3 install -r requirements.txt
+
+
+## Usage and options
+
+```
+usage: grobid-client.py [-h] [--input INPUT] [--config CONFIG]
+                        [--output OUTPUT] [--n N]
+                        service
+
+Client for GROBID services
+
+positional arguments:
+  service          one of [processFulltextDocument, processHeaderDocument,
+                   processReference]
+
+optional arguments:
+  -h, --help       show this help message and exit
+  --input INPUT    path to the directory containing PDF to process
+  --config CONFIG  path to the config file, default is ./config.json
+  --output OUTPUT  Path to the directory where to put the results
+  --n N            Concurrency for service usage
+```
+
+
+Example:
+
+> python3 grobid-client.py --input ~/tmp/in2 --output ~/tmp/out processFulltextDocument
+
+This command will process all the PDF files present in the input directory (file with extension `.pdf` only) with the `processFulltextDocument` service of GROBID, and write the resulting XML TEI files under the output directory, reusing the file name with a different file extension (`.tei.xml`), using the default `10` concurrent workers. 
+
+> python3 grobid-client.py --input ~/tmp/in2 --output ~/tmp/out --n 20 processHeaderDocument
+
+This command will process all the PDF files present in the input directory (file with extension `.pdf` only) with the `processHeaderDocument` service of GROBID, and write the resulting XML TEI files under the output directory, reusing the file name with a different file extension (`.tei.xml`), using `20` concurrent workers. 
+
+## Benchmarking
+
+Full text processing of __136 PDF__ (total 3443 pages, in average 25 pages per PDF) on Intel Core i7-4790K CPU 4.00GHz, 4 cores (8 threads), 16GB memory, n being the concurrency parameter:
+
+| n  | runtime (s)| s/PDF | 
+|----|------------|-------|
+| 1  | 209.0 | 1.       | 
+| 2  | 112.0 | 0.       |
+| 3  | 80.4  | 0.       |
+| 5  | 62.9  | 0.       |
+| 8  | 55.7  | 0.       |
+| 10 | 55.3  | 0.       |
+
+![Runtime Plot](resources/)
+
+As complementary info, GROBID processing of header, with of the 136 PDF and with `n=10` takes 3.74 s (15 times faster than the complete full text processing because only the two first pages of the PDF are considered). In similar conditions, extraction and structuring of bibliographical references takes 26.9 s.
+
+## Todo
+
+Benchmarking with more files (e.g. million ISTEX PDF). Also implement existing GROBID services for text input (date, name, affiliation/address, raw bibliographical references, etc.). Better support for parameters (including elements where to put coordinates).
+
+## License and contact
+
+Distributed under [Apache 2.0 license](http://www.apache.org/licenses/LICENSE-2.0). 
+
+Main author and contact: Patrice Lopez (<patrice.lopez@science-miner.com>)
---
+++ grobid-client.py
@@ -0,0 +1,121 @@
+import sys
+import os
+import io
+import json
+import argparse
+import time
+import concurrent.futures
+import glob
+from client import ApiClient
+import ntpath
+
+'''
+This version uses the standard ProcessPoolExecutor for parallelizing the concurrent calls to the GROBID services. 
+Given the limits of ThreadPoolExecutor (input stored in memory, blocking Executor.map until the whole input
+is acquired), it works with batches of PDF of a size indicated in the config.json file (default is 1000 entries). 
+We are moving from first batch to the second one only when the first is entirely processed - which means it is
+slightly sub-optimal, but should scale better. However acquiring a list of million of files in directories would
+require something scalable too, which is not implemented for the moment.   
+'''
+class grobid_client(ApiClient):
+
+    def __init__(self, config_path='./config.json'):
+        self.config = None
+        self._load_config(config_path)
+
+    def _load_config(self, path='./config.json'):
+        """
+        Load the json configuration 
+        """
+        config_json = open(path).read()
+        self.config = json.loads(config_json)
+
+    def process(self, input, output, n, service):
+        batch_size_pdf = self.config['batch_size']
+        pdf_files = []
+
+        for pdf_file in glob.glob(input + "/*.pdf"):
+            pdf_files.append(pdf_file)
+
+            if len(pdf_files) == batch_size_pdf:
+                self.process_batch(pdf_files, output, n, service)
+                pdf_files = []
+
+        # last batch
+        if len(pdf_files) > 0:
+            self.process_batch(pdf_files, output, n)
+
+    def process_batch(self, pdf_files, output, n):
+        print(len(pdf_files))
+        #with concurrent.futures.ThreadPoolExecutor(max_workers=n) as executor:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=n) as executor:
+            for pdf_file in pdf_files:
+                executor.submit(self.process_pdf, pdf_file, output, service)
+
+    def process_pdf(self, pdf_file, output, service):
+        print(pdf_file)
+        files = {
+            'input': (
+                pdf_file,
+                open(pdf_file, 'rb'),
+                'application/pdf',
+                {'Expires': '0'}
+            )
+        }
+        
+        res, status = self.post(
+            url='http://'+self.config['grobid_server']+":"+self.config['grobid_port']+"/api/"+service,
+            files=files,
+            headers={'Accept': 'text/plain'}
+        )
+
+        #print(str(status))
+        #print(res.text)
+
+        if status == 503:
+            time.sleep(5)
+            return self.process_pdf(pdf_file, output)
+        elif status != 200:
+            print('Processing failed with error ' + str(status))
+        else:
+            # writing TEI file
+            # we use ntpath here to be sure it will work on Windows too
+            pdf_file_name = ntpath.basename(pdf_file)
+            filename = os.path.join(output, os.path.splitext(pdf_file_name)[0] + '.tei.xml')
+            #print(filename)
+            with io.open(filename,'w',encoding='utf8') as tei_file:
+                tei_file.write(res.text)
+
+def test():
+    client = grobid_client()
+    # do some test...
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "Client for GROBID services")
+    parser.add_argument("service", help="one of [processFulltextDocument, processHeaderDocument, processReference]")
+    parser.add_argument("--input", default=None, help="path to the directory containing PDF to process") 
+    parser.add_argument("--config", default="./config.json", help="path to the config file, default is ./config.json") 
+    parser.add_argument("--output", default=None, help="Path to the directory where to put the results") 
+    parser.add_argument("--n", default=10, help="Concurrency for service usage") 
+
+    args = parser.parse_args()
+
+    input_path = args.input
+    config_path = args.config
+    output_path = args.output
+    n =1
+    try:
+        n = int(args.n)
+    except ValueError:
+        print("Invalid concurrency parameter n:", n, "n = 10 will be used by default")
+
+    service = args.service
+
+    client = grobid_client(config_path=config_path)
+
+    start_time = time.time()
+
+    client.process(input_path, output_path, n, service)
+
+    runtime = round(time.time() - start_time, 3)
+    print("runtime: %s seconds " % (runtime))

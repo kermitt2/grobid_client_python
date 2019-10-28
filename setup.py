5b6d62a84d418e11b44d93848f4afefee23d70ef
Nick Lothian <nick.lothian@tyto.ai>
2019-10-28 10:51:40 +1030

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='grobid-client-python',
+      version='0.0.1',
+      description='grobid-client-python',
+      author=' kermitt2',
+      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    )

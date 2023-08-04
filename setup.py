9d7581336cb61022c55b18401c1cb5031e307130
lopez <patrice.lopez@science-miner.com>
2023-08-04 12:47:46 +0200

    bump version in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='grobid_client_python',
-      version='0.0.5',
+      version='0.0.6',
       description='Simple python client for GROBID REST services',
       author='kermitt2',
       long_description=open("Readme.md", encoding='utf-8').read(),
